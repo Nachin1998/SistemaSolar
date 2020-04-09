4d8f887157e30fa9157afb257401ed839958ca88
Ignacio <Ignacio.f.l@hotmail.com>
2020-04-08 22:33:52 -0300

    Added planet manager. Planets now spaw in scene and rotate around the sun.

---
+++ SistemaSolar/Assets/PlanetManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetManager : MonoBehaviour
+{
+    //public GameObject planetToOrbit;
+    //public float orbitingSpeed;
+    //public bool rotatesOnOwnAxis = true;
+    //public float ownRotationSpeed;
+
+    public List<GameObject> planets = new List<GameObject>();
+
+    
+    void Start()
+    {
+        for (int i = 0; i < planets.Count; i++)
+        {
+            Instantiate(planets[i], planets[i].transform.position, Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- SistemaSolar/Assets/Scripts/PlanetOribting.cs
+++ SistemaSolar/Assets/Scripts/PlanetOribting.cs
@@ -1,8 +1,5 @@
-﻿using JetBrains.Annotations;
-using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
 
 
@@ -12,6 +9,9 @@ public class PlanetOribting : MonoBehaviour
     public float orbitingSpeed;
     public bool rotatesOnOwnAxis = true;
     public float ownRotationSpeed;
+
+    float actualPosX;
+    float actualPosZ;
 
     void Orbit(GameObject planet, float speed)
     {
